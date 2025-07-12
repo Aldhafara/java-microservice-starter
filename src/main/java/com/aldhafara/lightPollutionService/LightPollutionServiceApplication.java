fe2fe76dc00cfe65ff333c4bd9ae65c12e35fa69
Marek Nawrocki <aldhafara@onet.pl>
2025-07-12 20:48:18 +0200

    Add initial Spring Boot project structure

---
+++ src/main/java/com/aldhafara/lightPollutionService/LightPollutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aldhafara.lightPollutionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightPollutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightPollutionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldhafara/lightPollutionService/LightPollutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldhafara.lightPollutionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightPollutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
